fa51a06f93caf585a257145f94417dce232f2ed0
ben <ben.spoor@gmail.com>
2016-04-05 22:24:43 +0200

    Updated URL's

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,7 +63,9 @@ module.exports = function(grunt) {
           all: {
             options: {
               urls: [
-                'http://127.0.0.1:9999/index.html'
+                'http://127.0.0.1:9999/index.html',
+                'http://127.0.0.1:9999/test/tests.html',
+                'test/tests.html'
               ],
               browsers: browsers,
               build: process.env.TRAVIS_JOB_ID,
